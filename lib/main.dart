1394bdd8c1683008ddd9338044ddbf12bca85ce4
jocgomez <dev.jocgomez@gmail.com>
2023-08-11 12:59:02 -0500

    Added disapearing navigation rail for wide screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:animated_responsive_layout/destinations.dart';
 import 'package:animated_responsive_layout/models/models.dart';
 import 'package:animated_responsive_layout/widgets/widgets.dart';
 import 'package:flutter/material.dart';
@@ -38,44 +37,65 @@ class _FeedState extends State<Feed> {
       _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
 
   int selectedIndex = 0;
+  bool wideScreen = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final double width = MediaQuery.of(context).size.width;
+    wideScreen = width > 600;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: _backgroundColor,
-        child: EmailListView(
-          selectedIndex: selectedIndex,
-          onSelected: (index) {
-            setState(() {
-              selectedIndex = index;
-            });
-          },
-          currentUser: widget.currentUser,
-        ),
+      body: Row(
+        children: [
+          if (wideScreen)
+            DisappearingNavigationRail(
+              selectedIndex: selectedIndex,
+              backgroundColor: _backgroundColor,
+              onDestinationSelected: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
+          Expanded(
+            child: Container(
+              color: _backgroundColor,
+              child: EmailListView(
+                selectedIndex: selectedIndex,
+                onSelected: (index) {
+                  setState(() {
+                    selectedIndex = index;
+                  });
+                },
+                currentUser: widget.currentUser,
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: _colorScheme.tertiaryContainer,
-        foregroundColor: _colorScheme.onTertiaryContainer,
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
-      bottomNavigationBar: NavigationBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        destinations: destinations.map<NavigationDestination>((d) {
-          return NavigationDestination(
-            icon: Icon(d.icon),
-            label: d.label,
-          );
-        }).toList(),
-        selectedIndex: selectedIndex,
-        onDestinationSelected: (index) {
-          setState(() {
-            selectedIndex = index;
-          });
-        },
-      ),
+      floatingActionButton: wideScreen
+          ? null
+          : FloatingActionButton(
+              backgroundColor: _colorScheme.tertiaryContainer,
+              foregroundColor: _colorScheme.onTertiaryContainer,
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            ),
+      bottomNavigationBar: wideScreen
+          ? null
+          : DisappearingBottomNavigationBar(
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
     );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,6 @@
-export 'email_widget.dart';
+export 'disappearing_bottom_navigation_bar.dart';
+export 'disappearing_navigation_rail.dart';
 export 'email_list_view.dart';
+export 'email_widget.dart';
 export 'search_bar.dart';
 export 'star_button.dart';
